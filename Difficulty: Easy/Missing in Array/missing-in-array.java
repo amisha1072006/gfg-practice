27d5305aea50174566d4b18a073c8d910f08db72
amisha1072006 <amishapandey524@gmail.com>
2025-08-18 23:00:43 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Missing in Array/missing-in-array.java
+++ Difficulty: Easy/Missing in Array/missing-in-array.java
@@ -1,20 +1,20 @@
-import java.util.*;
+import java.util.Scanner;
 class Solution {
-   public static int missingNum(int arr[]) {
+    int missingNum(int arr[]) {
         // code here
-        int n = arr.length+1;
-        long total = (long) n*(n+1)/2;
-        long expect =0;
+        long n= arr.length+1;
+        long total = (n*(n+1))/2;
+        long expect = 0;
         for(int i=0;i<arr.length;i++){
             expect +=arr[i];
         }
-        long value = total - expect;
+        long value = total-expect;
         return (int) value;
-        }
-        public static void main(String[] args){
-            Solution sn = new Solution();
-            int[] arr = {1,2,3,5};
-            int v1 = sn.missingNum(arr);
-            System.out.println(v1);
-        }
     }
+    public static void main(String[] args){
+        Solution sn = new Solution();
+        int[] arr = {8,2,4,5,3,7,1};
+        int v1 = sn.missingNum(arr);
+        System.out.println(v1);
+    }
+}
