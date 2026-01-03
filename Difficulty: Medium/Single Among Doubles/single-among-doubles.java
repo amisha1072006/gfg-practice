f2ec90fa25b3d0ba7dad1f3613038c56bffa7c8b
Amisha Pandey <amishapandey524@gmail.com>
2026-01-03 22:04:10 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Single Among Doubles/single-among-doubles.java
@@ -0,0 +1,12 @@
+// User function Template for Java
+
+class Sol {
+    public static int search(int n, int arr[]) {
+    int ans = 0;
+    for(int i =0;i<n;i++){
+        ans = ans^arr[i];
+    }
+    return ans;
+        }
+    
+    }
