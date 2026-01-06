87c6041ed7f80c4cc3bd162df513017a9324ce98
Amisha Pandey <amishapandey524@gmail.com>
2026-01-06 23:45:52 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Array Duplicates/array-duplicates.java
+++ Difficulty: Easy/Array Duplicates/array-duplicates.java
@@ -1,16 +1,34 @@
 class Solution {
     public ArrayList<Integer> findDuplicates(int[] arr) {
+        //BRUTE FORCE APPROACH
+        
      ArrayList<Integer> list=new ArrayList<>();
-       HashSet<Integer> duplicate = new HashSet<>();
-       
-       for(int a : arr)
-       {
-           if(!duplicate.add(a))
-           {
-               list.add(a);
-           }
-       }
-       return list;
-       
+     int n = arr.length;
+  //for(int i =0;i<n;i++){
+    //       for(int j = i+1;j<n;j++){
+    //           if(arr[i] == arr[j] && !list.contains(arr[i])){
+    //               list.add(arr[i]);
+    //           }
+    //       }
+    //   }
+    // Collections.sort(list);
+    //   return list;
+    
+    //OPTMIZED APPROACH
+    int[] freq = new int[n+1];
+    for(int i =0;i<n;i++)
+{
+    freq[arr[i]]++;
+}
+for(int i=1;i<=n;i++){
+    if(freq[i] >1){
+        list.add(i);
+        
+    }
+    }
+    // if(list.isEmpty()){
+    //     list.add(-1);
+    //     }
+      return list;
 }
 }
