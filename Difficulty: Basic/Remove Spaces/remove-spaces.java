534c296ed2443486c5cc22e2bfbcebe21e1c1d0d
Amisha Pandey <amishapandey524@gmail.com>
2026-01-09 23:41:55 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Remove Spaces/remove-spaces.java
@@ -0,0 +1,10 @@
+// User function Template for Java
+class Solution {
+
+    String modify(String s) {
+        // your code here
+        String noSpaces = s.replaceAll("\\s+", "");
+        return noSpaces;
+    
+    }
+}
