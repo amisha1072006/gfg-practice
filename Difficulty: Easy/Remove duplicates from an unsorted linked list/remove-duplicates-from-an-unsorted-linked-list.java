59ac7ef312b58f855bd8353861387824bb33ea3e
Amisha Pandey <amishapandey524@gmail.com>
2026-01-10 23:55:53 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Remove duplicates from an unsorted linked list/remove-duplicates-from-an-unsorted-linked-list.java
@@ -0,0 +1,34 @@
+/* The structure of linked list is the following
+class Node
+{
+    int data;
+    Node next;
+    Node(int d) {
+        data = d;
+        next = null;
+    }
+}
+*/
+
+class Solution {
+    // Function to remove duplicates from unsorted linked list.
+    public Node removeDuplicates(Node head) {
+        // Your code here
+                if (head == null) return null;
+        java.util.HashSet<Integer> seen = new java.util.HashSet<>();
+        Node current = head;
+        Node prev = null;
+
+        while (current != null) {
+            if (seen.contains(current.data)) {
+                prev.next = current.next;
+            } else {
+                seen.add(current.data);
+                prev = current;
+            }
+            current = current.next;
+        }
+        return head;
+    
+    }
+}
