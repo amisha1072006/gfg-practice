5041c52d00c3e395a6b4292d441f0d3afdd2ae63
Amisha Pandey <amishapandey524@gmail.com>
2026-04-16 21:29:59 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Form a palindrome/form-a-palindrome.java
+++ Difficulty: Medium/Form a palindrome/form-a-palindrome.java
@@ -1,22 +1,19 @@
-// User function Template for Java
-
 class Solution {
-    static int countMin(String str) {
+    int findMinInsertions(String s) {
         // code here
-                int n= str.length();
-        StringBuilder sb = new StringBuilder(str);
-        String str2 = sb.reverse().toString();
-        int dp[][]= new int[n+1][n+1];
+         int n = s.length();
         
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=n;j++){
-                if(str.charAt(i-1)==str2.charAt(j-1)){
-                    dp[i][j]=1+dp[i-1][j-1];
-                }
-                else dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
-            }
+        int dp[][] = new int[n+1][n+1];
+        
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=n; j++){
+              if(s.charAt(i-1)==s.charAt(n-j)) dp[i][j] = dp[i-1][j-1]+1;
+              else dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+            }    
         }
-        int palidromelen = dp[n][n];
-        return n-palidromelen;
+        
+        int maxLen = dp[n][n];
+        
+        return n-maxLen;
     }
 }
