b93ac85e4ca1b6ec6d83e720a4bee53cf95fd211
Amisha Pandey <amishapandey524@gmail.com>
2026-08-25 20:10:04 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Peak element/peak-element.java
+++ Difficulty: Medium/Peak element/peak-element.java
@@ -1,27 +1,17 @@
 class Solution {
     public int peakElement(int[] arr) {
         // code here
-                int n= arr.length;
-        if(n==1)
-            return 0;
-        if(arr[0]>arr[1])
-            return 0;
-        
-        if(arr[n-1]>arr[n-2])
-            return n-1;
-        
-        int low=1, high=n-2, mid=0;
-        while(low<=high)
-        {
-            mid= (low+high)/2;
-            if(arr[mid]>arr[mid-1] && arr[mid]>arr[mid+1])
-                return mid;
-            if(arr[mid-1]<arr[mid] && arr[mid]<arr[mid+1])
-                low=mid+1;
-            else
-                high=mid-1;
+        int start = 0 , end = arr.length-1;
+        while(start<end){
+            int mid = (start+end)/2;
+            if(arr[mid]<arr[mid+1]){
+                start = mid+1;
+            }else{
+                end = mid;
+            }
+
+
         }
-        
-        return -1;
+         return start;
     }
 }
